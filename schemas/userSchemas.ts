4c48be8012282144d9477db68c9dd624ad0c2404
skyispy <skyispy1003@gmail.com>
2025-08-28 16:34:46 +0900

    [refactor] userSchemas.ts - refine 사용안해서 제거

--- schemas/userSchemas.ts
+++ schemas/userSchemas.ts
@@ -14,10 +14,6 @@ export const UserSignupSchema = z.object({
   }),
   confirmPassword: z.string(),
 })
-.refine((data) => data.password === data.confirmPassword, {
-  message: '비밀번호가 일치하지 않습니다.',
-  path: ['confirmPassword'],
-})
 .required();
 
 export type UserSignupFields = Partial<z.infer<typeof UserSignupSchema>>;
