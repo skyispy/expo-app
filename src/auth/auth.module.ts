97ed6dbf0e46248ed2612e5b492890df310da31e
skyispy <skyispy1003@gmail.com>
2025-09-17 16:26:18 +0900

    [refactor] auth 모듈 설정 수정 및 의존성/구성 개선

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,20 +3,20 @@ import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
-import { UserService } from '../user/user.service';
 import { RefreshTokensEntity } from './models';
-import { UserEntity } from '../user/models';
 import { JwtStrategy } from './strategies';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
     JwtModule.register({
       secret: 'secret_key',
     }),
-    TypeOrmModule.forFeature([UserEntity, RefreshTokensEntity]),
+    TypeOrmModule.forFeature([RefreshTokensEntity]),
+    UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, UserService, JwtStrategy],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
